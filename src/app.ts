0d9d6916c59fd765c2c1c09ed3b595dbda4c0e31
Ali Raisi <aliraisi97@outlook.com>
2026-06-12 20:08:40 +0330

    fix: make app modular

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express from 'express';
+import express, { type Express } from 'express';
 import helmet from 'helmet';
 import { pinoHttp } from 'pino-http';
 import routes from './routes.js';
@@ -7,18 +7,23 @@ import { logger } from './config/logger.js';
 import { errorMiddleware } from './middlewares/error-handler.middleware.js';
 import { notFoundMiddleware } from './middlewares/not-found.middleware.js';
 
-export const app = express();
+export const createApp = (): Express => {
+  const app = express();
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(
-  pinoHttp({
-    logger,
-  }),
-);
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(
+    pinoHttp({
+      logger,
+    }),
+  );
 
-app.use('/api/v1', routes);
+  app.use('/api/v1', routes);
 
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+  app.use(notFoundMiddleware);
+  app.use(errorMiddleware);
+
+  return app;
+};
